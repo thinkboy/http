fe693d6eaf54fa522085db08baaa68ceba4d1386
thinkboy <liuding_234@yahoo.com.cn>
2013-11-21 19:15:28 +0800

    optimize code

--- parse/parse.go
+++ parse/parse.go
@@ -70,22 +70,24 @@ func ParseUrlParam(values url.Values, stru interface{}) error {
 
 		//fmt.Printf("field \"%s\", value: \"%s\"\n", stField.Name, value)
 
+		if value == "" {
+			if ti.OmitEmpty == false {
+				return errors.New(fmt.Sprintf("field \"%s\" is empty", stField.Name))
+			}
+
+			continue
+		}
+
 		switch svField.Kind() {
 		case reflect.String:
-			if value == "" {
-				if ti.OmitEmpty == false {
-					return errors.New(fmt.Sprintf("field \"%s\" is empty", stField.Name))
+			if ti.RangeStr != "" {
+				num, err := strconv.Atoi(ti.RangeStr)
+				if err != nil {
+					return errors.New(fmt.Sprintf("invalid tag range \"%s\" error: %v", ti.RangeStr, err))
 				}
-			} else {
-				if ti.RangeStr != "" {
-					num, err := strconv.Atoi(ti.RangeStr)
-					if err != nil {
-						return errors.New(fmt.Sprintf("invalid tag range \"%s\" error: %v", ti.RangeStr, err))
-					}
 
-					if len(value) != num {
-						return errors.New(fmt.Sprintf("field \"%s\" is not %d character", stField.Name, num))
-					}
+				if len(value) != num {
+					return errors.New(fmt.Sprintf("field \"%s\" is not %d character", stField.Name, num))
 				}
 			}
 
@@ -96,131 +98,113 @@ func ParseUrlParam(values url.Values, stru interface{}) error {
 				e    error
 			)
 
-			if value == "" {
-				if ti.OmitEmpty == false {
-					return errors.New(fmt.Sprintf("field \"%s\" is empty", stField.Name))
+			if ti.RangeStr != "" {
+				num, err := strconv.Atoi(ti.RangeStr)
+				if err != nil {
+					return errors.New(fmt.Sprintf("invalid tag range \"%s\" error: %v", ti.RangeStr, err))
 				}
-			} else {
-				if ti.RangeStr != "" {
-					num, err := strconv.Atoi(ti.RangeStr)
-					if err != nil {
-						return errors.New(fmt.Sprintf("invalid tag range \"%s\" error: %v", ti.RangeStr, err))
-					}
 
-					data, e = hex.DecodeString(value)
-					if e != nil {
-						return errors.New(fmt.Sprintf("invalid tag \"%s\" error: %v", ti.RangeStr, err))
-					}
+				data, e = hex.DecodeString(value)
+				if e != nil {
+					return errors.New(fmt.Sprintf("invalid tag \"%s\" error: %v", ti.RangeStr, err))
+				}
 
-					if len(data) != num {
-						return errors.New(fmt.Sprintf("field \"%s\" is not %d bytes", stField.Name, num))
-					}
+				if len(data) != num {
+					return errors.New(fmt.Sprintf("field \"%s\" is not %d bytes", stField.Name, num))
 				}
 			}
 
 			svField.SetBytes(data)
 		case reflect.Int:
-			if value == "" {
-				if ti.OmitEmpty == false {
-					return errors.New(fmt.Sprintf("field \"%s\" is empty", stField.Name))
-				}
-			} else {
-				val, err := strconv.Atoi(value)
-				if err != nil {
-					return errors.New(fmt.Sprintf("invalid int value \"%s\" error: %v", value, err))
-				}
-
-				// Verify range
-				if ti.RangeStr != "" {
-					index := strings.Index(ti.RangeStr, ":")
-					if index == -1 {
-						return errors.New(fmt.Sprintf("invalid range tag \"%s\"", ti.RangeStr))
-					}
-
-					minStr := ti.RangeStr[:index]
-
-					if minStr != "" {
-						min, err := strconv.Atoi(minStr)
-						if err != nil {
-							return errors.New(fmt.Sprintf("invalid tag range \"%s\" error: %v", ti.RangeStr, err))
-						}
-
-						if val < min {
-							return errors.New(fmt.Sprintf("field \"%s\" out of range", stField.Name))
-						}
-					}
-
-					maxStr := ti.RangeStr[index+1:]
-
-					if maxStr != "" {
-						max, err := strconv.Atoi(maxStr)
-						if err != nil {
-							return errors.New(fmt.Sprintf("invalid tag range \"%s\" error: %v", ti.RangeStr, err))
-						}
-
-						if val > max {
-							return errors.New(fmt.Sprintf("field \"%s\" out of range", stField.Name))
-						}
-					}
-
-				}
-
-				// It is converted to 32 bits inside SetInt()
-				svField.SetInt(int64(val))
+			val, err := strconv.Atoi(value)
+			if err != nil {
+				return errors.New(fmt.Sprintf("invalid int value \"%s\" error: %v", value, err))
 			}
+
+			// Verify range
+			if ti.RangeStr != "" {
+				index := strings.Index(ti.RangeStr, ":")
+				if index == -1 {
+					return errors.New(fmt.Sprintf("invalid range tag \"%s\"", ti.RangeStr))
+				}
+
+				minStr := ti.RangeStr[:index]
+
+				if minStr != "" {
+					min, err := strconv.Atoi(minStr)
+					if err != nil {
+						return errors.New(fmt.Sprintf("invalid tag range \"%s\" error: %v", ti.RangeStr, err))
+					}
+
+					if val < min {
+						return errors.New(fmt.Sprintf("field \"%s\" out of range", stField.Name))
+					}
+				}
+
+				maxStr := ti.RangeStr[index+1:]
+
+				if maxStr != "" {
+					max, err := strconv.Atoi(maxStr)
+					if err != nil {
+						return errors.New(fmt.Sprintf("invalid tag range \"%s\" error: %v", ti.RangeStr, err))
+					}
+
+					if val > max {
+						return errors.New(fmt.Sprintf("field \"%s\" out of range", stField.Name))
+					}
+				}
+
+			}
+
+			// It is converted to 32 bits inside SetInt()
+			svField.SetInt(int64(val))
 
 		case reflect.Int64:
-			if value == "" {
-				if ti.OmitEmpty == false {
-					return errors.New(fmt.Sprintf("field \"%s\" is empty", stField.Name))
-				}
-			} else {
-				val, err := strconv.ParseInt(value, 10, 64)
-				if err != nil {
-					return errors.New(fmt.Sprintf("invalid int value \"%s\" error: %v", value, err))
-				}
-
-				// Verify range
-				if ti.RangeStr != "" {
-					index := strings.Index(ti.RangeStr, ":")
-					if index == -1 {
-						return errors.New(fmt.Sprintf("invalid range tag \"%s\"", ti.RangeStr))
-					}
-
-					minStr := ti.RangeStr[:index]
-
-					if minStr != "" {
-						min, err := strconv.ParseInt(minStr, 10, 64)
-						if err != nil {
-							return errors.New(fmt.Sprintf("invalid tag range \"%s\" error: %v", ti.RangeStr, err))
-						}
-
-						if val < min {
-							return errors.New(fmt.Sprintf("field \"%s\" out of range", stField.Name))
-						}
-					}
-
-					maxStr := ti.RangeStr[index+1:]
-
-					if maxStr != "" {
-						max, err := strconv.ParseInt(maxStr, 10, 64)
-						if err != nil {
-							return errors.New(fmt.Sprintf("invalid tag range \"%s\" error: %v", ti.RangeStr, err))
-						}
-
-						if val > max {
-							return errors.New(fmt.Sprintf("field \"%s\" out of range", stField.Name))
-						}
-					}
-
-				}
-
-				svField.SetInt(val)
+			val, err := strconv.ParseInt(value, 10, 64)
+			if err != nil {
+				return errors.New(fmt.Sprintf("invalid int value \"%s\" error: %v", value, err))
 			}
+
+			// Verify range
+			if ti.RangeStr != "" {
+				index := strings.Index(ti.RangeStr, ":")
+				if index == -1 {
+					return errors.New(fmt.Sprintf("invalid range tag \"%s\"", ti.RangeStr))
+				}
+
+				minStr := ti.RangeStr[:index]
+
+				if minStr != "" {
+					min, err := strconv.ParseInt(minStr, 10, 64)
+					if err != nil {
+						return errors.New(fmt.Sprintf("invalid tag range \"%s\" error: %v", ti.RangeStr, err))
+					}
+
+					if val < min {
+						return errors.New(fmt.Sprintf("field \"%s\" out of range", stField.Name))
+					}
+				}
+
+				maxStr := ti.RangeStr[index+1:]
+
+				if maxStr != "" {
+					max, err := strconv.ParseInt(maxStr, 10, 64)
+					if err != nil {
+						return errors.New(fmt.Sprintf("invalid tag range \"%s\" error: %v", ti.RangeStr, err))
+					}
+
+					if val > max {
+						return errors.New(fmt.Sprintf("field \"%s\" out of range", stField.Name))
+					}
+				}
+
+			}
+
+			svField.SetInt(val)
+		default:
 			//TODO:need to add more type later
 			//case reflect.Float32:
 			//case reflect.Float64:
-		default:
 			return errors.New(fmt.Sprintf("field \"%s\" no this type \"%s\" deal", stField.Name))
 
 		}
@@ -282,27 +266,29 @@ func ParseBodyParam(rBody io.ReadCloser, stru interface{}) (body []byte, erro er
 		// Find values
 		value, _ := values[strings.ToLower(stField.Name)]
 
+		if value == "" {
+			if ti.OmitEmpty == false {
+				erro = errors.New(fmt.Sprintf("field \"%s\" is empty", stField.Name))
+				return
+			}
+
+			continue
+		}
+
 		//fmt.Printf("field \"%s\", value: \"%s\"\n", stField.Name, value)
 
 		switch svField.Kind() {
 		case reflect.String:
-			if value == "" {
-				if ti.OmitEmpty == false {
-					erro = errors.New(fmt.Sprintf("field \"%s\" is empty", stField.Name))
+			if ti.RangeStr != "" {
+				num, err := strconv.Atoi(ti.RangeStr)
+				if err != nil {
+					erro = errors.New(fmt.Sprintf("invalid tag range \"%s\" error: %v", ti.RangeStr, err))
 					return
 				}
-			} else {
-				if ti.RangeStr != "" {
-					num, err := strconv.Atoi(ti.RangeStr)
-					if err != nil {
-						erro = errors.New(fmt.Sprintf("invalid tag range \"%s\" error: %v", ti.RangeStr, err))
-						return
-					}
 
-					if len(value) != num {
-						erro = errors.New(fmt.Sprintf("field \"%s\" is not %d character", stField.Name, num))
-						return
-					}
+				if len(value) != num {
+					erro = errors.New(fmt.Sprintf("field \"%s\" is not %d character", stField.Name, num))
+					return
 				}
 			}
 
@@ -313,149 +299,128 @@ func ParseBodyParam(rBody io.ReadCloser, stru interface{}) (body []byte, erro er
 				e    error
 			)
 
-			if value == "" {
-				if ti.OmitEmpty == false {
-					erro = errors.New(fmt.Sprintf("field \"%s\" is empty", stField.Name))
+			if ti.RangeStr != "" {
+				num, err := strconv.Atoi(ti.RangeStr)
+				if err != nil {
+					erro = errors.New(fmt.Sprintf("invalid tag range \"%s\" error: %v", ti.RangeStr, err))
 					return
 				}
-			} else {
-				if ti.RangeStr != "" {
-					num, err := strconv.Atoi(ti.RangeStr)
-					if err != nil {
-						erro = errors.New(fmt.Sprintf("invalid tag range \"%s\" error: %v", ti.RangeStr, err))
-						return
-					}
 
-					data, e = hex.DecodeString(value)
-					if e != nil {
-						erro = errors.New(fmt.Sprintf("invalid tag \"%s\" error: %v", ti.RangeStr, err))
-						return
-					}
+				data, e = hex.DecodeString(value)
+				if e != nil {
+					erro = errors.New(fmt.Sprintf("invalid tag \"%s\" error: %v", ti.RangeStr, err))
+					return
+				}
 
-					if len(data) != num {
-						erro = errors.New(fmt.Sprintf("field \"%s\" is not %d bytes", stField.Name, num))
-						return
-					}
+				if len(data) != num {
+					erro = errors.New(fmt.Sprintf("field \"%s\" is not %d bytes", stField.Name, num))
+					return
 				}
 			}
 
 			svField.SetBytes(data)
 		case reflect.Int:
-			if value == "" {
-				if ti.OmitEmpty == false {
-					erro = errors.New(fmt.Sprintf("field \"%s\" is empty", stField.Name))
-					return
-				}
-			} else {
-				val, err := strconv.Atoi(value)
-				if err != nil {
-					erro = errors.New(fmt.Sprintf("invalid int value \"%s\" error: %v", value, err))
+			val, err := strconv.Atoi(value)
+			if err != nil {
+				erro = errors.New(fmt.Sprintf("invalid int value \"%s\" error: %v", value, err))
+				return
+			}
+
+			// Verify range
+			if ti.RangeStr != "" {
+				index := strings.Index(ti.RangeStr, ":")
+				if index == -1 {
+					erro = errors.New(fmt.Sprintf("invalid range tag \"%s\"", ti.RangeStr))
 					return
 				}
 
-				// Verify range
-				if ti.RangeStr != "" {
-					index := strings.Index(ti.RangeStr, ":")
-					if index == -1 {
-						erro = errors.New(fmt.Sprintf("invalid range tag \"%s\"", ti.RangeStr))
+				minStr := ti.RangeStr[:index]
+
+				if minStr != "" {
+					min, err := strconv.Atoi(minStr)
+					if err != nil {
+						erro = errors.New(fmt.Sprintf("invalid tag range \"%s\" error: %v", ti.RangeStr, err))
 						return
 					}
 
-					minStr := ti.RangeStr[:index]
-
-					if minStr != "" {
-						min, err := strconv.Atoi(minStr)
-						if err != nil {
-							erro = errors.New(fmt.Sprintf("invalid tag range \"%s\" error: %v", ti.RangeStr, err))
-							return
-						}
-
-						if val < min {
-							erro = errors.New(fmt.Sprintf("field \"%s\" out of range", stField.Name))
-							return
-						}
+					if val < min {
+						erro = errors.New(fmt.Sprintf("field \"%s\" out of range", stField.Name))
+						return
 					}
-
-					maxStr := ti.RangeStr[index+1:]
-
-					if maxStr != "" {
-						max, err := strconv.Atoi(maxStr)
-						if err != nil {
-							erro = errors.New(fmt.Sprintf("invalid tag range \"%s\" error: %v", ti.RangeStr, err))
-							return
-						}
-
-						if val > max {
-							erro = errors.New(fmt.Sprintf("field \"%s\" out of range", stField.Name))
-							return
-						}
-					}
-
 				}
 
-				// It is converted to 32 bits inside SetInt()
-				svField.SetInt(int64(val))
+				maxStr := ti.RangeStr[index+1:]
+
+				if maxStr != "" {
+					max, err := strconv.Atoi(maxStr)
+					if err != nil {
+						erro = errors.New(fmt.Sprintf("invalid tag range \"%s\" error: %v", ti.RangeStr, err))
+						return
+					}
+
+					if val > max {
+						erro = errors.New(fmt.Sprintf("field \"%s\" out of range", stField.Name))
+						return
+					}
+				}
+
 			}
 
+			// It is converted to 32 bits inside SetInt()
+			svField.SetInt(int64(val))
 		case reflect.Int64:
-			if value == "" {
-				if ti.OmitEmpty == false {
-					erro = errors.New(fmt.Sprintf("field \"%s\" is empty", stField.Name))
-					return
-				}
-			} else {
-				val, err := strconv.ParseInt(value, 10, 64)
-				if err != nil {
-					erro = errors.New(fmt.Sprintf("invalid int value \"%s\" error: %v", value, err))
+			val, err := strconv.ParseInt(value, 10, 64)
+			if err != nil {
+				erro = errors.New(fmt.Sprintf("invalid int value \"%s\" error: %v", value, err))
+				return
+			}
+
+			// Verify range
+			if ti.RangeStr != "" {
+				index := strings.Index(ti.RangeStr, ":")
+				if index == -1 {
+					erro = errors.New(fmt.Sprintf("invalid range tag \"%s\"", ti.RangeStr))
 					return
 				}
 
-				// Verify range
-				if ti.RangeStr != "" {
-					index := strings.Index(ti.RangeStr, ":")
-					if index == -1 {
-						erro = errors.New(fmt.Sprintf("invalid range tag \"%s\"", ti.RangeStr))
+				minStr := ti.RangeStr[:index]
+
+				if minStr != "" {
+					min, err := strconv.ParseInt(minStr, 10, 64)
+					if err != nil {
+						erro = errors.New(fmt.Sprintf("invalid tag range \"%s\" error: %v", ti.RangeStr, err))
 						return
 					}
 
-					minStr := ti.RangeStr[:index]
-
-					if minStr != "" {
-						min, err := strconv.ParseInt(minStr, 10, 64)
-						if err != nil {
-							erro = errors.New(fmt.Sprintf("invalid tag range \"%s\" error: %v", ti.RangeStr, err))
-							return
-						}
-
-						if val < min {
-							erro = errors.New(fmt.Sprintf("field \"%s\" out of range", stField.Name))
-							return
-						}
+					if val < min {
+						erro = errors.New(fmt.Sprintf("field \"%s\" out of range", stField.Name))
+						return
 					}
-
-					maxStr := ti.RangeStr[index+1:]
-
-					if maxStr != "" {
-						max, err := strconv.ParseInt(maxStr, 10, 64)
-						if err != nil {
-							erro = errors.New(fmt.Sprintf("invalid tag range \"%s\" error: %v", ti.RangeStr, err))
-							return
-						}
-
-						if val > max {
-							erro = errors.New(fmt.Sprintf("field \"%s\" out of range", stField.Name))
-							return
-						}
-					}
-
 				}
 
-				svField.SetInt(val)
+				maxStr := ti.RangeStr[index+1:]
+
+				if maxStr != "" {
+					max, err := strconv.ParseInt(maxStr, 10, 64)
+					if err != nil {
+						erro = errors.New(fmt.Sprintf("invalid tag range \"%s\" error: %v", ti.RangeStr, err))
+						return
+					}
+
+					if val > max {
+						erro = errors.New(fmt.Sprintf("field \"%s\" out of range", stField.Name))
+						return
+					}
+				}
+
 			}
+
+			svField.SetInt(val)
+
+		default:
 			//TODO:need to add more type later
 			//case reflect.Float32:
 			//case reflect.Float64:
-		default:
 			erro = errors.New(fmt.Sprintf("field \"%s\" no this type \"%s\" deal", stField.Name))
 			return
 
